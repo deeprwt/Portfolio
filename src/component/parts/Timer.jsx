f44ece6b5f606fa03e1225285a725fe88bb9df0f
Deepak rawat <rwtroshan28@gmail.com>
2022-09-23 19:47:11 +0530

    updated new deploing code

--- src/component/parts/Timer.jsx
+++ src/component/parts/Timer.jsx
@@ -1,34 +1,12 @@
 import React from "react";
 
-const Timer = () => {
-  const [number, setNumber] = React.useState(2);
-  const incrNum = () => {
-    setNumber(number * 2);
-  };
-  if (number <= 400) {
-    setInterval(incrNum, 1000);
-  } else {
-    setNumber(400);
-  }
-  console.log(number);
+import CountUp from "react-countup";
 
+const Timer = () => {
   return (
-    <div>
-      <h1>
-        Fist data
-        <p id="counter" data-target="4000">
-          {number}
-        </p>
-      </h1>
-      <h1>
-        second data
-        <p id="counter" data-target="15000"></p>
-      </h1>
-      <h1>
-        third data
-        <p id="counter" data-target="8000"></p>
-      </h1>
-    </div>
+    <>
+      <div className=""></div>
+    </>
   );
 };
 
